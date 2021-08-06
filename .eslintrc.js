f30269023d3a9c395a213eceecf50edec12d254d
TakahashiJumpei <b1014098@gmail.com>
2021-08-07 04:46:35 +0900

    JSのコードを検証するために、eslint-loaderを追加

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2020: true,
+    },
+    parserOptions: {
+      sourceType: 'module',
+    },
+    extends: ['eslint:recommended'],
+    rules: {
+      'prefer-const': 'error',
+    },
+  };
+  
